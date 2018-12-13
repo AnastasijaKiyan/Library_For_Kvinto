e9d7af81298dd465f08def58ff8ee9f52d00318b
Nastia <anastasijakiyan@gmail.com>
2018-12-13 16:44:17 +0200

    commjit11

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -2,31 +2,30 @@
 // const path = require(" path");
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-const SRC_DIR = __dirname + '/src';
-const DIST_DIR = __dirname + '/build';
+const SRC_DIR = __dirname + "/src";
+const DIST_DIR = __dirname + "/build";
 
 module.exports = {
-  entry: [
-    SRC_DIR + '/index.tsx'
-  ],
+  entry: [SRC_DIR + "/index.tsx"],
   output: {
     path: DIST_DIR,
-    filename: 'bundle.min.js'
+    publicPath: '/',
+    filename: "bundle.min.js"
   },
-  mode: "production",
+  mode: "development",
   module: {
     rules: [
       {
         test: /\.(ts|tsx)$/,
         // exclude: /node_modules/,
-        use:  ['babel-loader', 'ts-loader']
+        use: ["babel-loader", "ts-loader"]
       },
       {
         test: /\.(sass|css)$/,
         // exclude: /node_modules/,
-        use:  ExtractTextPlugin.extract({
+        use: ExtractTextPlugin.extract({
           fallback: "style-loader",
-          use: ['css-loader', 'sass-loader']
+          use: ["css-loader", "sass-loader"]
         })
       }
     ]
@@ -34,7 +33,8 @@ module.exports = {
   resolve: {
     extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
-  plugins: [
-    new ExtractTextPlugin("./style/style.css"),
-  ]
+  plugins: [new ExtractTextPlugin("./style/style.css")],
+  devServer: {
+    contentBase: DIST_DIR
+  }
 };
